c80c9f48d45c6b2e50ed14eaad10a31d74ba8eb8
Laurent Blondy <laurent.blondy1@gmail.com>
2020-04-30 13:14:34 +0200

    1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const chalk = require('chalk');
 const dashboardRoutes = require('./routes/dashboardRoutes.js');
 const ticketRoutes = require('./routes/ticketRoutes.js');
 
-const app = express();
+const app = express();//test
 
 app
 	.use(cors())
@@ -24,4 +24,4 @@ mongoose
 	.then(() => console.log(chalk.bold.blue('# MongoDB Connected !')))
 	.catch(err => console.log(err));
 
-app.listen(3000, () => console.log('http://localhost:3000'));
+app.listen(3000, () => console.log('http://localhost:3000')); \
