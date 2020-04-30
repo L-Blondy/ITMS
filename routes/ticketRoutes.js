4073f6d5861826853ae73d3310d07dfbb0952b76
Laurent Blondy <laurent.blondy1@gmail.com>
2020-04-30 13:14:13 +0200

    1

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -8,33 +8,41 @@ const sendData = (req, res) => {
 	res.send(req.data);
 };
 
-router.get('/new', TicketController.getBlankTicket, sendData);
+router.get(
+	'/new',
+	TicketController.getBlankTicket,
+	sendData
+);
 
-router.post('/new', TicketController.saveNewTicket, sendData);
+router.get(
+	'/:id',
+	TicketController.getTicket,
+	sendData
+);
 
-router.get('/:id', TicketController.getTicket, sendData);
+router.get(
+	'/:id/subscribe',
+	(req, res) => LiveUpdate.subscribe(req, res)
+);
 
-router.post('/:id', TicketController.updateTicket, (req, res) => LiveUpdate.dispatch(req, res));
+router.post(
+	'/new',
+	TicketController.saveNewTicket,
+	sendData
+);
+
+router.post(
+	'/:id',
+	TicketController.updateTicket,
+	(req, res) => LiveUpdate.dispatch(req, res)
+);
 
 router.post(
 	'/:id/attach',
 	TicketController.uploadFile.single('file'),
 	TicketController.saveFile,
-	sendData
+	(req, res) => LiveUpdate.dispatch(req, res)
 );
 
-router.get('/:id/subscribe', (req, res) => LiveUpdate.subscribe(req, res));
 
 module.exports = router;
-
-
-// send file:
-
-// const file = req.data;
-
-// 		const mimetype = file.mimetype;
-// 		const b64 = new Buffer.from(file.data).toString('base64');
-// 		const src = `data:${ mimetype };base64,${ b64 }`;
-// 		const name = file.name;
-
-// 		res.send({ src, name });
