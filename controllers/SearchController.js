97b3d4acd638bd70eb1b074110a1534237a28b5f
Laurent Blondy <laurent.blondy1@gmail.com>
2020-06-01 10:50:35 +0200

    1

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -1,37 +1,39 @@
-require('dotenv').config();
-const path = require('path');
-const Search = require('../models/search/Search');
-const fs = require('fs');
+require( 'dotenv' ).config();
+const path = require( 'path' );
+const Search = require( '../models/search/Search' );
+const fs = require( 'fs' );
 
 module.exports = {
 
-	handleSearch: async function (req, res, next) {
+	handleSearch: async function ( req, res, next ) {
 		const { type } = req.params;
-		console.log(req.query, type);
+		console.log( req.query, type );
 		try {
-			const search = new Search(type);
-			const data = await search.find(req.query);
+			const search = new Search( type );
+			const data = await search.find( req.query );
 			const resultsCount = await search.count();
 
 			req.data = {
 				results: data,
 				resultsCount,
-				skipped: parseInt(req.query.skip),
+				startFrom: parseInt( req.query.startFrom ),
+				query: req.query
 			};
 			next();
 		}
-		catch (err) {
-			console.log(err);
+		catch ( err ) {
+			console.log( err );
 			req.data = {
 				results: [],
 				resultsCount: 0,
 				skipped: 0,
+				query: req.query
 			};
 			next();
 		}
 	},
 
-	sendData: function (req, res) {
-		res.send({ searchData: req.data });
+	sendData: function ( req, res ) {
+		res.send( { searchData: req.data } );
 	}
 };
--- models/search/Search.js
+++ models/search/Search.js
@@ -1,19 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const Incident = require('../ticket/Incident');
-const Request = require('../ticket/Request');
-const Change = require('../ticket/Change');
+const fs = require( 'fs' );
+const path = require( 'path' );
+const Incident = require( '../ticket/Incident' );
+const Request = require( '../ticket/Request' );
+const Change = require( '../ticket/Change' );
 
 class Search {
 
-	constructor(type) {
+	constructor ( type ) {
 		this.type = type;
 		this.model = this._getModel();
 		this.searchParams = {};
 	}
 
-	_getModel() {
-		switch (this.type) {
+	_getModel () {
+		switch ( this.type ) {
 			case 'incidents':
 				return Incident;
 			case 'requests':
@@ -23,59 +23,59 @@ class Search {
 		}
 	}
 
-	async find(query) {
-		if (!query.skip)
-			query.skip = 0;
-		if (!query.limit)
+	async find ( query ) {
+		if ( !query.startFrom )
+			query.startFrom = 1;
+		if ( !query.limit )
 			query.limit = 20;
 
-		this.searchParams = Object.entries(query).reduce((searchParams, entry) => {
+		this.searchParams = Object.entries( query ).reduce( ( searchParams, entry ) => {
 			let [ prop, value ] = entry;
 			const searchANumber = prop === 'escalation';
-			const searchADate = prop.isOneOf([ 'createdOn', 'updatedOn', 'dueDate' ]);
+			const searchADate = prop.isOneOf( [ 'createdOn', 'updatedOn', 'dueDate' ] );
 
 			//skip 'page'
-			if (prop.isOneOf([ 'skip', 'limit', 'sort' ]))
+			if ( prop.isOneOf( [ 'startFrom', 'limit', 'sort' ] ) )
 				return searchParams;
 			//prop is stored as a number
-			if (searchANumber) {
+			if ( searchANumber ) {
 				searchParams[ prop ] = value;
 			}
 			//search a date
-			else if (searchADate) {
-				value = dateToNum(value);
-				console.log(value);
+			else if ( searchADate ) {
+				value = dateToNum( value );
+				console.log( value );
 				searchParams[ prop ] = { $gte: value, $lte: value + 86400000 }; //date < results < date + 1 day
 			}
 			//search a string or a numbe rwithin a string
 			else {
-				value = value.split(' ').join('\\s'); //handling white space 
-				searchParams[ prop ] = { $regex: new RegExp(value), $options: 'i' };
+				value = value.split( ' ' ).join( '\\s' ); //handling white space 
+				searchParams[ prop ] = { $regex: new RegExp( value ), $options: 'i' };
 			}
 			return searchParams;
-		}, {});
+		}, {} );
 
 		const sort = query.sort || { sortBy: 'id', sortOrder: -1 };
 
 		return await this.model
-			.find(this.searchParams)
-			.skip(parseInt(query.skip))
-			.limit(parseInt(query.limit))
-			.sort({ [ sort[ 'sortBy' ] ]: sort[ 'sortOrder' ] });
+			.find( this.searchParams )
+			.skip( parseInt( query.startFrom ) - 1 )
+			.limit( parseInt( query.limit ) )
+			.sort( { [ sort[ 'sortBy' ] ]: sort[ 'sortOrder' ] } );
 	}
 
-	async count() {
-		return await this.model.countDocuments(this.searchParams);
+	async count () {
+		return await this.model.countDocuments( this.searchParams );
 	}
 }
 
 module.exports = Search;
 
-function dateToNum(date) {
-	date = date.split('/');
+function dateToNum ( date ) {
+	date = date.split( '/' );
 	[ date[ 0 ], date[ 1 ] ] = [ date[ 1 ], date[ 0 ] ];
-	date = date.join('/');
-	const num = new Date(date).getTime();
+	date = date.join( '/' );
+	const num = new Date( date ).getTime();
 	return num;
 }
 
